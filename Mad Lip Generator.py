1a241aebcce382a7654035bc17c0efdeee49891c
Marcos Matos <64489185+marcosjmatos@users.noreply.github.com>
2020-10-10 20:42:15 -0500

    Add files via upload

---
+++ Mad Lip Generator.py
@@ -0,0 +1,68 @@
+#MAD LIB GENERATOR
+
+#3 jugadas
+
+
+
+#Pregunta las palabras
+Adj = input("Escribe un adjetivo: ")
+Sus = input("Escribe un sustantivo: ")
+Game =input("Escribe un Juego: ")
+Verbo1 = input("Escribe un Verbo en presente continuo: ")
+Sus_plu = input("Escribe un sustantivo en Plural: ")
+Plan = input("Escribe una planta: ")
+Parte_cu = input("Escribe una parte del cuerpo: ")
+lug = input("Escribe un lugar: ")
+num =  input("Escribe un numero: ")
+Adj_2 = input("Escribe otro adjetivo: ")
+Sus_2 = input("Escribe otro sustantivo: ")
+Sus_plu2 = input("Escribe otro sustantivo en plural: ")
+Verbo2 = input("Escribe otro Verbo en presente continuo: ")
+Sus_plu3 = input("Escribe otro sustantivo en plural: ")
+Verbo3 = input("Escribe otro Verbo en presente continuo: ")
+Sus_3 = input("Escribe otro sustantivo: ")
+Verbo4 = input("Escribe un Verbo en presente: ")
+Adj_3 = input("Escribe un adjetivo Plural: ")
+Sus_plu4 = input("Y para terminar escribe un ultimo sustantivo en plural: ")
+
+#Imprime el relato con las palabras
+print("_______________________________________________________________________________________________________________________")
+print("Una vacación es cuando tu haces un viaje a algún ", Adj, " lugar" )
+print("con tu ", Adj_2, " Familia, usualmente tu vas a algún que está cerca a un", Sus_2)
+print("Un buen lugar para vacacionar es uno donde tu puedas montar ", Sus_plu)
+print("jugar ", Game, " o cazar ", Sus_plu2, " . A mi me gusta pasar tiempo ", Verbo1, " o", Verbo2)
+print("Cuando mis padres van de vacaciones, ellos pasan tiempo comiendo", Sus_plu3, " tres veces al dia")
+print("los padres juegan golf y las madres se sientan", Verbo3, ". En el verano pasado, mi hermano cayo em un/una", Sus)
+print(" y una", Plan, " le enveneno su", Parte_cu, " . Mi familia esta yendo a", lug, " , y yo voy a", Verbo4)
+print("Los padres necesitan las vacaciones mas que los niños porque los padres estan siempre ", Adj_3)
+print("y por eso ellos tienen que trabajar", num, " horas cada dia, durante todo el año para haciendo suficientes", Sus_plu4)
+print("para pagar las vacaciones")
+print("_______________________________________________________________________________________________________________________")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
